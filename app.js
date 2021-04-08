effadf196b1c3e30ada55da371de752d3c82bd57
PhilipsCalvinKosno <72179377+PhilipsCalvinKosno@users.noreply.github.com>
2021-04-08 18:11:52 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express')
+
+const PORT = 8000
+const HOST = '0.0.0.0'
+
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Nama = Philips Calvin Kosno
+              NIM = 51018016
+              Jurusan = Sistem informasi\n')
+})
+
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
